d098e0617346a6ce7bd32a1e48d0eca28e32eee5
Yang Zhao <yzhao@csod.com>
2020-10-31 16:26:51 +1300

    Save

---
+++ .prettierrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    arrowParens: 'always',
+    bracketSpacing: true,
+    embeddedLanguageFormatting: 'auto',
+    htmlWhitespaceSensitivity: 'css',
+    insertPragma: false,
+    jsxBracketSameLine: false,
+    jsxSingleQuote: false,
+    printWidth: 80,
+    proseWrap: 'preserve',
+    quoteProps: 'as-needed',
+    requirePragma: false,
+    semi: true,
+    useTabs: false,
+    vueIndentScriptAndStyle: false,
+
+    // custom properties
+    trailingComma: 'all',
+    tabWidth: 4,
+    singleQuote: true,
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,71 @@
-import React from "react";
-import "./App.css";
-import { Input } from "antd";
+import React from 'react';
+import './App.css';
+import { Button, Col, Input, Row } from 'antd';
+import { UploadOutlined } from '@ant-design/icons';
 
 function App() {
-    const [file, setFile] = React.useState("");
+    const [filePath, setFilePath] = React.useState('');
+    const [content, setContent] = React.useState('');
     React.useEffect(() => {
-        chrome.storage.local.get((obj) => setFile(obj.path || ""));
+        setContent(window.localStorage.getItem('content') || '');
+        chrome?.storage?.local?.get((obj) => {
+            setFilePath(obj.path || '');
+            setContent(obj.content || '');
+        });
     }, []);
-    const setFilePath = (path: string) => {
-        setFile(path);
-        chrome.storage.local.set({ path });
+    const setFile = (file?: File) => {
+        if (file) {
+            setFilePath(file.name);
+            const fileReader = new FileReader();
+            fileReader.onloadend = (e) => {
+                const text = e.target?.result;
+                if (typeof text === 'string') {
+                    setContent(text);
+                }
+            };
+            fileReader.readAsText(file);
+        }
+    };
+    const onSave = () => {
+        if (chrome?.storage?.local) {
+            chrome.storage.local.set({ path: filePath, content });
+        } else {
+            window.localStorage.setItem('content', content);
+        }
     };
     return (
         <div className="App">
-            <div>
-                <Input
-                    placeholder="JS file path"
-                    value={file}
-                    onChange={(e) => setFilePath(e.target.value)}
-                />
+            <div style={{ margin: '10px' }}>
+                <Row gutter={24}>
+                    <Col span="12">
+                        <div style={{ background: '#fafafa' }}>
+                            <label htmlFor="upload">
+                                <UploadOutlined style={{ fontSize: '2em' }} />
+                            </label>
+                            <Input
+                                id="upload"
+                                hidden
+                                type="file"
+                                onChange={(e) => setFile(e.target?.files?.[0])}
+                            />
+                        </div>
+                    </Col>
+                </Row>
+                <Row gutter={24}>
+                    <Col span="12">
+                        <Input.TextArea
+                            style={{ width : '80%' }}
+                            rows={5}
+                            value={content}
+                            onChange={(e) => setContent(e.target.value)}
+                        />
+                    </Col>
+                </Row>
+                <Row gutter={24}>
+                    <Col span="12">
+                        <Button type="primary" onClick={onSave} block>Save</Button>
+                    </Col>
+                </Row>
             </div>
         </div>
     );
